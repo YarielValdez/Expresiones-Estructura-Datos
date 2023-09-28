acf61c85511e2cad2b8fcf3b60484c95312b8b75
YarielValdez <yarvatri@gmail.com>
2023-09-27 21:50:57 -0600

    Replace variables cambiado a un solo codigo, arreglos en metodo main

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,15 @@
 import java.util.*;
+
 public class Main {
-    static String inf,prefix,post, operator = "+-*/^";
+    static String inf, prefix, post, operator = "+-*/^";
     static int last = 0;
-    static boolean end = false;
     static HashMap<Character, String> Values = new HashMap<>();
 
     public static boolean validacion() {
         boolean val = true, operand = true;
         int par = 0;
         for (int i = 0; i < inf.length(); i++) {
-            if (inf.charAt(i) == 32)
-                continue;
+            if (inf.charAt(i) == 32) continue;
             if (operand) {
                 if (inf.charAt(i) >= 97 && inf.charAt(i) <= 122) {
                     operand = false;
@@ -18,19 +17,15 @@ public class Main {
                 } else if (inf.charAt(i) == 40) {
                     par++;
                     last = inf.charAt(i);
-                } else
-                    val = false;
+                } else val = false;
             } else {
                 if (operator.contains(Character.toString(inf.charAt(i)))) {
                     operand = true;
                     last = inf.charAt(i);
                 } else if ((inf.charAt(i) == 41)) {
-                    if (last == 40)
-                        val = false;
-                    else
-                        par--;
-                } else
-                    val = false;
+                    if (last == 40) val = false;
+                    else par--;
+                } else val = false;
             }
             if (!val) {
                 break;
@@ -49,8 +44,7 @@ public class Main {
         for (int i = 0; i < inf.length(); i++) {
             char caracter = inf.charAt(i);
 
-            if (caracter == 32)
-                continue;
+            if (caracter == 32) continue;
 
             if (Character.isLetterOrDigit(caracter)) {
                 resultado.append(caracter);
@@ -84,8 +78,7 @@ public class Main {
         for (int i = inf.length() - 1; i >= 0; i--) {
             char caracter = inf.charAt(i);
 
-            if (caracter == 32)
-                continue;
+            if (caracter == 32) continue;
 
             if (Character.isLetterOrDigit(caracter)) {
                 expresionInvertida.append(caracter);
@@ -124,7 +117,6 @@ public class Main {
     }
 
     public static void error() {
-        if (!end) {
             System.out.println("Errores encontrados:");
 
             if (inf.contains("()")) {
@@ -159,36 +151,6 @@ public class Main {
             }
             if (operator.contains(Character.toString(inf.charAt(inf.length() - 1))))
                 System.out.println("Error, la expresión termina en un operador");
-
-        } else {
-            System.out.println("La expresión está bien escrita");
-        }
-    }
-
-    public static String replaceVariablesPos() {
-        StringBuilder replaced = new StringBuilder();
-        for (int i = 0; i < pos().length(); i++) {
-            char c = pos().charAt(i);
-            if (Character.isLetter(c) && Values.containsKey(c)) {
-                replaced.append(Values.get(c));
-            } else {
-                replaced.append(c);
-            }
-        }
-        return replaced.toString();
-    }
-
-    public static String replaceVariablesPre() {
-        StringBuilder replaced = new StringBuilder();
-        for (int i = 0; i < prefija().length(); i++) {
-            char c = prefija().charAt(i);
-            if (Character.isLetter(c) && Values.containsKey(c)) {
-                replaced.append(Values.get(c));
-            } else {
-                replaced.append(c);
-            }
-        }
-        return replaced.toString();
     }
 
     public static void main(String[] args) {
@@ -199,61 +161,18 @@ public class Main {
             inf = sc.nextLine();
             inf = inf.toLowerCase();
             end = validacion();
-            if (!end) {
-                System.out.println("Errores encontrados:");
-
-
-            error();
-            System.out.println("Expresión postfija: " + pos());
-            System.out.println("Expresión prefija: " + prefija());
-            valueOf();
-            String var = pos();
-            replaceVariables();
-
-            System.out.println("Expresión infija con valores: " + replaceVariables());
-            System.out.println("Expresión postfija con valores: " + replaceVariablesPos());
-            System.out.println("Expresión prefija con valores: " + replaceVariablesPre());
-
-                if (inf.contains("()")) {
-                    System.out.println("Error: Paréntesis vacío");
-                }
-
-                if (!validacionParentesis()) {
-                    System.out.println("Error: Paréntesis mal colocados");
-                }
-
-                if (inf.matches(".*[\\+\\-\\*/\\^]{2,}.*")) {
-                    System.out.println("Error: Dos operadores juntos");
-                }
-
-                if (inf.matches(".*[a-z0-9][a-z0-9].*")) {
-                    System.out.println("Error: Dos operandos juntos");
-                }
-
-                if (inf.length() > 0 && !Character.isLetter(inf.charAt(0)) && inf.charAt(0) != '(') {
-                    System.out.println("Error: La expresión inicia con un operando diferente de paréntesis de apertura");
-                }
-
-                for (int i = 0; i < inf.length(); i++) {
-                    char c = inf.charAt(i);
-                    if (!Character.isLetter(c) && c != '(' && c != ')' && c != '+' && c != '-' && c != '*' && c != '/' && c != '^') {
-                        System.out.println("Error: Caracter no válido en la posición " + (i + 1));
-                    }
-                }
-
-                if (inf.matches(".*\\d[a-z].*")) {
-                    System.out.println("Error: Se escribieron números en lugar de letras");
-                }
-                if (operator.contains(Character.toString(inf.charAt(inf.length() - 1))))
-                    System.out.println("Error, la expresión termina en un operador");
-
-            } else {
+            if (!end)
+               error();
+            else {
                 System.out.println("La expresión está bien escrita");
                 prefix = prefija();
                 post = pos();
                 System.out.println("Expresión prefija: " + prefix);
                 System.out.println("Expresión postfija: " + post);
                 valueOf();
+                System.out.println("Expresión infija con valores: " + replaceVariables(inf));
+                System.out.println("Expresión postfija con valores: " + replaceVariables(post));
+                System.out.println("Expresión prefija con valores: " + replaceVariables(prefix));
                 System.out.println(EvalPost());
             }
         }
@@ -279,27 +198,24 @@ public class Main {
         for (int i = 0; i < inf.length(); i++) {
             char caracter = inf.charAt(i);
             if (caracter >= 'a' && caracter <= 'z' && !Values.containsKey(caracter)) {
-                    System.out.println("Ingrese el valor de " + caracter);
-                    Values.put(caracter, sc.nextLine());
+                System.out.println("Ingrese el valor de " + caracter);
+                Values.put(caracter, sc.nextLine());
             }
         }
     }
 
-
-    public static String replaceVariables() {
-        StringBuilder replaced = new StringBuilder();
-        for (int i = 0; i < inf.length(); i++) {
-            char c = inf.charAt(i);
-            if (Character.isLetter(c) && Values.containsKey(c)) {
-                replaced.append(Values.get(c));
-            } else {
-                replaced.append(c);
-            }
+    public static String replaceVariables(String formula){
+        StringBuilder reemplazo = new StringBuilder();
+        for(int i=0;i<formula.length();i++){
+            char c = formula.charAt(i);
+            if(c>='a' && c<='z')
+                reemplazo.append(Values.get(c));
+            else
+                reemplazo.append(c);
         }
-        return replaced.toString();
+        return reemplazo.toString();
     }
-
-    public static int eval(String operator,String n2, String n1){
+    public static int eval(String operator, String n2, String n1) {
         int num1 = Integer.parseInt(n1);
         int num2 = Integer.parseInt(n2);
         return switch (operator) {
@@ -312,20 +228,20 @@ public class Main {
         };
     }
 
-    public static String EvalPost(){
+    public static String EvalPost() {
         Stack<String> funcion = new Stack<>();
         Stack<String> evaluacion = new Stack<>();
-        for(int i=post.length()-1;i>=0;i--){
+        for (int i = post.length() - 1; i >= 0; i--) {
             char elemento = post.charAt(i);
-            if(operator.contains(Character.toString(elemento)))
+            if (operator.contains(Character.toString(elemento)))
                 funcion.push(Character.toString(elemento));
-            else{
+            else {
                 funcion.push(Values.get(elemento));
             }
         }
-        while(!funcion.empty()){
-            if(operator.contains(funcion.peek()))
-                evaluacion.push(eval(funcion.pop(),evaluacion.pop(),evaluacion.pop())+"");
+        while (!funcion.empty()) {
+            if (operator.contains(funcion.peek()))
+                evaluacion.push(eval(funcion.pop(), evaluacion.pop(), evaluacion.pop()) + "");
             else
                 evaluacion.push(funcion.pop());
         }
